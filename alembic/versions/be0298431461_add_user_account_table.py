24eee19f53761001bb067ffb729dd391c0ac4dca
Ekaterina Senenkova <esenenkova@g,ail.com>
2023-08-25 21:53:49 +0200

    add alembc

---
+++ alembic/versions/be0298431461_add_user_account_table.py
@@ -0,0 +1,31 @@
+"""Add user_account table
+
+Revision ID: be0298431461
+Revises: 
+Create Date: 2023-08-25 21:50:56.336122
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'be0298431461'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'user_account',
+        sa.Column('user_id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('name', sa.String(64), nullable=False),
+        sa.Column('email', sa.Unicode(256)),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('user_account')
