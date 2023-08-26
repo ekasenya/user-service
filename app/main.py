a76b8f817d22ffcbe91f3425a327b589c88f7a2b
Ekaterina Senenkova <esenenkova@g,ail.com>
2023-08-26 23:06:01 +0200

    add create user endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+import os
+
+import uvicorn
+from fastapi import FastAPI, Depends
+
+from app.core.deps import get_user_repository
+from app.repositories.user_repository import UserRepository
+from app.schemas.user import UserInfo, User
 
 app = FastAPI()
+
+
+config = {
+    'DB_NAME': os.environ.get('DB_NAME', 'user-db'),
+    'DB_HOST': os.environ['DB_HOST'],
+    'DB_PORT': os.environ['DB_PORT'],
+    'GREETING': os.environ.get('GREETING', 'Hello'),
+}
 
 
 @app.get('/')
@@ -11,3 +26,12 @@ async def root():
 @app.get('/health')
 async def check_health():
     return {"status": "OK"}
+
+
+@app.post('/user', response_model=User)
+async def create_user(user_info: UserInfo, user_repository: UserRepository = Depends(get_user_repository)):
+    return await user_repository.create_user(name=user_info.name, email=user_info.email)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8080, log_level="info")
