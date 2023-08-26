1ca730fedab510a8fe6566335841ea32b8ba3e06
Ekaterina Senenkova <esenenkova@g,ail.com>
2023-08-26 23:04:40 +0200

    add repositories

---
+++ app/repositories/base_sql_alchemy_repository.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.asyncio import AsyncConnection
+
+class BaseSqlAlchemyRepository:
+    def __init__(self, db_connection: AsyncConnection):
+        super().__init__()
+        self._db_connection = db_connection
---
+++ app/repositories/user_repository.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from sqlalchemy import insert
+from app.repositories.base_sql_alchemy_repository import BaseSqlAlchemyRepository
+from app.models.user import user_account
+from app.schemas.user import User, UserInfo
+
+
+class UserRepository(BaseSqlAlchemyRepository):
+    async def create_user(
+            self,
+            name: str,
+            email: Optional[str]
+    ) -> User:
+        insert_command = insert(user_account).values(
+            name=name,
+            email=email
+        ).returning(user_account)
+
+        async with self._db_connection.begin():
+            row = (await self._db_connection.execute(insert_command)).one()
+
+        return User(
+            user_id=row.user_id,
+            user_info=UserInfo(
+                name=row.name, email=row.email
+            )
+        )
